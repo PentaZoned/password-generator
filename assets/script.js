892b1b5b64e2cd52c3c9429fd3e8ae62b43505d3
Bradley Le <brabrele@gmail.com>
2021-12-27 15:03:41 -0800

    added comments to javascript code template, added README template

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
-// Assignment Code
+// References the ID 'generate' via a variable
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function is used to transfer the generated password to the text area 
 function writePassword() {
+  // The output of generatePassword function is assigned to the password variable as a string
   var password = generatePassword();
+  // Creates a reference variable used to show the generated password to the user
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  // Assigns the password variable to the reference variable
+  passwordText.value = password; 
 }
 
-// Add event listener to generate button
+// Allows the button to be used for interation and starts the js functions
 generateBtn.addEventListener("click", writePassword);
