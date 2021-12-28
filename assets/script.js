321606cd1e647fa6fe9cd16acb96ed3cc68cbea9
Bradley Le <brabrele@gmail.com>
2021-12-27 21:58:08 -0800

    add comments on the javascript

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ function writePassword() {
   passwordText.value = password; 
 }
 
-
+// Function will create the password
 function generatePassword() {
   // Character arrays
   const lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -19,12 +19,11 @@ function generatePassword() {
   const numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   // Excluded \ because it was making a weird syntax error.
   const specialArray = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "`"];
-
+  // This empty array will have the other arrays pushed to it depending on user selection
   var charArray = [];
 
-
-  // Password length requirement
-  var numChar = prompt("Enter a number from 8 to 128 for the number of characters.", "Ex. 20");
+  // Asks the user for the length of characters in numeric value
+  var numChar = prompt("Enter a number from 8 to 128 for the number of characters.", "21");
 
   // If user inputs a number not between 8 and 128, loop until user input is between 8 and 128
   while (numChar < 8 || numChar > 128) {
@@ -32,6 +31,8 @@ function generatePassword() {
     var numChar = prompt("Enter a number from 8 to 128 for the number of characters.");
     }
 
+  // The following confirm methods asks the user to press "OK" for true or "Cancel"
+  // Each if statement will test the boolean to decide whether to add the const char arrays to the final array or not
   var lowercase = confirm("Do you want lowercase letters in the password?");
   if (lowercase === true) {
     charArray.push(...lowerArray);
@@ -52,20 +53,22 @@ function generatePassword() {
     charArray.push(...specialArray);
   }
 
+  // Creates an empty string to hold the password
+  var passTemplate = "";
+
   // Displays error message when no character options are selected
   if (lowercase === false && uppercase === false && numeric === false && specialCharacters === false) {
     alert("Password cannot be created without any characters options.");
   }
 
-  numChar = Number(numChar);
-  var passTemplate = "";
   for (var i = 0; i < numChar; i++) {
+    // Formula will randomly pick an integer from 0 to the length of charArray
     var k = Math.floor((Math.random() * charArray.length));
+    // k will be passed as the argument for calling the element in the array
     passTemplate = passTemplate.concat(charArray[k]);
   }
   return passTemplate;
 }
-
 
 // Allows the button to be used for interation and starts the js functions
 generateBtn.addEventListener("click", writePassword);
